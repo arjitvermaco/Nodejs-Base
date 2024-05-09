0168f5548c9cb0756deedd0645923ef134206e9f
Arjit <offcampus012@gmail.com>
2024-05-10 02:04:08 +0530

    jwt install

---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1]; // Bearer Token
+    if (!token) {
+        return res.status(403).json({ message: 'A token is required for authentication' });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid Token' });
+    }
+    return next();
+};
+
+export default verifyToken;
